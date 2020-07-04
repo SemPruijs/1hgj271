6cbbbf792312990383393c39c3c7270b3af73f10
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-07-04 21:12:37 +0200

    Add project

---
+++ Assets/Scripts/Managers/DisplayManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayManager : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject inGame;
+    public GameObject playAgain;
+    public GameObject credit;
+    
+    void Update()
+    {
+        menu.SetActive(GameManager.Instance.state == GameManager.State.Menu);
+        inGame.SetActive(GameManager.Instance.state == GameManager.State.InGame);
+        playAgain.SetActive(GameManager.Instance.state == GameManager.State.PlayAgain);
+        credit.SetActive(GameManager.Instance.state == GameManager.State.Credit);
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using EZCameraShake;
+
+public class GameManager : MonoBehaviour
+{
+    public State state;
+    private static GameManager _instance;
+         public static GameManager Instance {
+         get {
+             return _instance;
+         }
+    }
+    private void Awake()
+    {
+         if (_instance != null && _instance != this)
+         {
+            Destroy(this.gameObject);
+         } else {
+            _instance = this;
+         } 
+    }
+
+    public enum State
+    {
+        InGame,
+        Menu,
+        PlayAgain,
+        Credit
+    };
+
+    void Start()
+    {
+        Menu();
+        // CameraShaker.Instance.ShakeOnce(3f, 1f, 3f, 3f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadCurrentScene();
+        }
+    }
+
+    public void Menu()
+    {
+        state = State.Menu;
+    }
+
+    public void InGame()
+    {
+        state = State.InGame;
+    }
+
+    public void Credit()
+    {
+        state = State.Credit;
+    }
+    
+    public void ReloadCurrentScene() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
